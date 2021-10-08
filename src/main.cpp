706b285e94b5bd67375fb8a60bf80615c54992de
garik816 <garikpost@rambler.ru>
2021-10-08 16:46:30 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,50 @@
 #include <Arduino.h>
+#include <LowPower.h>
+// #include "LowPower.h"
 #include <SoftwareSerial.h>
+#include <DHT.h>
 
-static unsigned long timer = millis();
-static unsigned long radioTimer = millis();
+#define DHT22_PIN 6
 
-char data = 0;
+DHT dht(DHT22_PIN, DHT22);
 
-SoftwareSerial radio(4, 3); // TX, RX
+int humidity = 0;
+float temperature = 0;
+String data;
 
-void blink(void){
-  if (millis() - timer > 1000) {
-    timer = millis();
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  }
-}
+int value = 0;
+float voltage;
 
-void transmit(void){
-  radio.write(data);
-  data++;
-}
+SoftwareSerial radio(8, 9); // TX, RX
 
 void radioSend(void){
-  if (millis() - radioTimer > 2000) {
-    radioTimer = millis();
-    transmit();
-  }
+    data = String("C01,") + "T=" + String(temperature) + ",H=" + String(humidity) + ",V=" + String(voltage) + ";";
+    radio.println(data);
+    Serial.println(data);
+}
+
+void batteryRead(void){
+  value = analogRead(A3);
+  voltage = value * 4.075/1023;
 }
 
 void setup() {
   Serial.begin(9600);
   radio.begin(9600);
+  dht.begin();
+  analogReference(INTERNAL);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  blink();
+  digitalWrite(LED_BUILTIN, HIGH);
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  batteryRead();
   radioSend();
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  // delay(500);
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  // LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
 }
