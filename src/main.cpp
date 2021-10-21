1eac4bdd08395cdcabd63d6740dadea8d7bce821
garik816 <garikpost@rambler.ru>
2021-10-21 13:15:05 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
-#include <LowPower.h>
-// #include "LowPower.h"
+#include <GyverPower.h>
 #include <SoftwareSerial.h>
 #include <DHT.h>
 
@@ -30,6 +29,8 @@ void batteryRead(void){
 }
 
 void setup() {
+  power.calibrate(power.getMaxTimeout());
+  power.setSleepMode(POWERDOWN_SLEEP);
   Serial.begin(9600);
   radio.begin(9600);
   dht.begin();
@@ -60,7 +61,6 @@ void loop() {
   digitalWrite(RadioSetPin, HIGH);
   delay(50);
 
-  // delay(500);
-  // LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+  power.sleepDelay(60000);
+
 }
